b8635b89870f70ac5569997c43ed68ee9c9752ac
Resi Wicaksono <resiwicaksono98@gmail.com>
2024-07-16 11:50:35 +0700

    setup base

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#5F2B8C',
+        'primary-hover': '#4A2370',
+        secondary: '#49B247',
+        'gray-darken': '#656565',
+        'gray-soft': '#BDBCBC'
+      }
+    }
   },
   plugins: []
 }
